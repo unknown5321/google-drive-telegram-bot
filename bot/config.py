396cdc49480919cee732409aabd355f47f603d60
unknown5321 <118245278+unknown5321@users.noreply.github.com>
2022-11-18 13:52:31 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,8 +6,8 @@ class config:
     SUDO_USERS = "637471660" # Sepearted by space.
     SUPPORT_CHAT_LINK = ""
     DOWNLOAD_DIRECTORY = "./downloads/"
-    G_DRIVE_CLIENT_ID = "57081522315-fv2ilarj10rvn9kk042aciqk357f2b4s.apps.googleusercontent.com"
-    G_DRIVE_CLIENT_SECRET = "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"
+    G_DRIVE_CLIENT_ID = "444204332864-36gr73o4ka7499tt0imh5kvb9git243s.apps.googleusercontent.com"
+    G_DRIVE_CLIENT_SECRET = "N1-4XVGkFXLPJ0ng4fRoHONk"
 
 
 class BotCommands:
